403742e535096272799893182961cc70d49d288d
hien dep trai <nguyenvanhiencntt3@gmail.com>
2023-02-24 16:22:46 +0700

    test ngrx-router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EffectsModule } from '@ngrx/effects';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
@@ -12,7 +13,10 @@ import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    data: {
+      test: 'hehe'
+    }
   },
   {
     path: 'gallery',
@@ -28,9 +32,10 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({router: routerReducer}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { map } from 'rxjs';
 import { GalleryFacade } from 'src/app/gallery/store/gallery.facade';
+import { selectRouteData } from 'src/app/router.selectors';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   private galleryFacade = inject(GalleryFacade);
+  private store = inject(Store);
   cats = this.galleryFacade.selectedGalleries$.pipe(
     map((selectedCats: Map<string, any>) => {
       if (!selectedCats) {
@@ -17,6 +20,10 @@ export class HomeComponent {
       return Array.from(selectedCats?.values())
     })
   ) as any;
+
+  ngOnInit() {
+    this.store.select(selectRouteData).subscribe(res => console.log(res))
+  }
 
   // Use normal Ngrx
   // private store = inject(Store);
